a6249882c708e4a3b509cfc3433ca48982cbb164
Eric Mulligan <eric.pierre.mulligan@gmail.com>
2016-06-17 07:52:21 -0400

    Mapping JSON API.

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,37 @@
-declare namespace JsonApi {
-
+interface JsonApi {
+  version?:string;
+  meta?:any;
 }
+
+interface JsonApiLinks {
+  next?:string;
+  last?:string;
+  related?:JsonApiRelatedResourceLink;
+  self?:string;
+  pagination?:JsonApiPaginationLinks;
+}
+
+interface JsonApiRelatedResourceLink {
+  href?:string;
+  meta?:any;
+}
+
+interface JsonApiPaginationLinks {
+  first:string;
+  last:string;
+  prev:string;
+  next:string;
+}
+
+interface JsonApiData {
+  
+}
+
+interface JsonApiDocument {
+  links?:JsonApiLinks;
+  errors?:Array<any>;
+  meta?:any;
+  jsonapi?:JsonApi;
+}
+
+
